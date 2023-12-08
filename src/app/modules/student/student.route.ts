fc6211a1a4cc7de34e4bcc2ef9cd25921d6d554c
heynazrul <heynazrul@gmail.com>
2023-12-09 00:01:35 +0600

    added user itnerface and model

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -1,13 +1,14 @@
 import express from 'express';
-import { StudentController } from './student.controller';
+import { StudentControllers } from './student.controller';
 
 const router = express.Router();
 
-//will call controller function
-router.post('/create-student', StudentController.createStudent);
+router.post('/create-student', StudentControllers.createStudent);
 
-router.get('/', StudentController.getAllStudents);
+router.get('/:studentId', StudentControllers.getSingleStudent);
 
-router.get('/:studentId', StudentController.getSingleStudent);
+router.delete('/:studentId', StudentControllers.deleteStudent);
+
+router.get('/', StudentControllers.getAllStudents);
 
 export const StudentRoutes = router;
